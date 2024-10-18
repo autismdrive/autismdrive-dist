98743e1406c0f2008d9a6652f0d2db1f97670584
Aaron Louie <ajlouie@gmail.com>
2024-10-18 12:47:15 -0400

    Adds instructions and comments to the deployment/update scripts

--- prepare_for_deploy.sh
+++ prepare_for_deploy.sh
@@ -1,20 +1,48 @@
 #!/usr/bin/env bash
 
-# Clear out what we have now
+###################################################################################################
+# WARNING: Run this script from your local development environment, not on the Production server!
+#
+# Deletes the current frontend and backend directories and replaces them with the latest
+# built versions of the frontend and backend. This script is intended to be run from the
+# autismdrive-dist directory.
+#
+# Assumes this script is in a directory called `autismdrive-dist`
+# and that the directory ../autismdrive exists and contains the
+# autismdrive/autismdrive repository, like so:
+#    ./
+#    ├── autismdrive/
+#    └── autismdrive-dist/
+#        ├── prepare_for_deploy.sh
+#        ├── backend/
+#        ├── frontend/
+#        └── ...
+#
+# After running this script, commit the changes to the `autismdrive-dist` git repository.
+# See the README file in this repository for further deployment instructions.
+###################################################################################################
+
+# Clear out old directories
 rm -rf backend/
 rm -rf frontend/
 
-# Assumes this is next to the star-drive checkout.
-cd ../star-drive/frontend
+# Build the frontend and backend from the source code
+cd ../autismdrive/frontend
 ng build --prod -c production
-cd ../../star-drive-dist/
-cp -r ../star-drive/frontend/dist/star-drive/ frontend/
-cp -r ../star-drive/backend backend
+cd ../../autismdrive-dist/
+
+# Copy the built frontend and backend into the autismdrive-dist directory
+cp -r ../autismdrive/frontend/dist/autismdrive/ frontend/
+cp -r ../autismdrive/backend backend
+
+# Remove unnecessary files and directories
 rm -rf backend/python-env
 rm -rf backend/.idea
 rm -rf backend/instance
 rm -rf backend/tests
 rm -rf star_drive.log
 rm -rf __pycache__/
+
+# Add the new frontend and backend to the git staging area
 git add backend
 git add frontend
